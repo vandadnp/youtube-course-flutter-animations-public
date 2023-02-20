2b0428132041856d32702f5c5808403535e71015
Vandad Nahavandipoor <vandad.np@gmail.com>
2023-02-20 05:11:44 +0100

    example4

---
+++ example4/android/app/src/main/kotlin/se/pixolity/example4/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.pixolity.example4
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example4/lib/main.dart
@@ -0,0 +1,164 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+@immutable
+class Person {
+  final String name;
+  final int age;
+  final String emoji;
+
+  const Person({
+    required this.name,
+    required this.age,
+    required this.emoji,
+  });
+}
+
+const people = [
+  Person(name: 'John', age: 20, emoji: '🙋🏻‍♂️'),
+  Person(name: 'Jane', age: 21, emoji: '👸🏽'),
+  Person(name: 'Jack', age: 22, emoji: '🧔🏿‍♂️'),
+];
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('People'),
+      ),
+      body: ListView.builder(
+        itemCount: people.length,
+        itemBuilder: (context, index) {
+          final person = people[index];
+          return ListTile(
+            onTap: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => DetailsPage(
+                    person: person,
+                  ),
+                ),
+              );
+            },
+            leading: Hero(
+              tag: person.name,
+              child: Text(
+                person.emoji,
+                style: const TextStyle(
+                  fontSize: 40,
+                ),
+              ),
+            ),
+            title: Text(person.name),
+            subtitle: Text(
+              '${person.age} years old',
+            ),
+            trailing: const Icon(
+              Icons.arrow_forward_ios,
+            ),
+          );
+        },
+      ),
+    );
+  }
+}
+
+class DetailsPage extends StatelessWidget {
+  final Person person;
+
+  const DetailsPage({
+    super.key,
+    required this.person,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Hero(
+          flightShuttleBuilder: (
+            flightContext,
+            animation,
+            flightDirection,
+            fromHeroContext,
+            toHeroContext,
+          ) {
+            switch (flightDirection) {
+              case HeroFlightDirection.push:
+                return Material(
+                  color: Colors.transparent,
+                  child: ScaleTransition(
+                    scale: animation.drive(
+                      Tween<double>(
+                        begin: 0.0,
+                        end: 1.0,
+                      ).chain(
+                        CurveTween(
+                          curve: Curves.fastOutSlowIn,
+                        ),
+                      ),
+                    ),
+                    child: toHeroContext.widget,
+                  ),
+                );
+              case HeroFlightDirection.pop:
+                return Material(
+                  color: Colors.transparent,
+                  child: fromHeroContext.widget,
+                );
+            }
+          },
+          tag: person.name,
+          child: Text(
+            person.emoji,
+            style: const TextStyle(
+              fontSize: 50,
+            ),
+          ),
+        ),
+      ),
+      body: Center(
+        child: Column(
+          children: [
+            const SizedBox(height: 20),
+            Text(
+              person.name,
+              style: const TextStyle(fontSize: 20),
+            ),
+            const SizedBox(height: 20),
+            Text(
+              '${person.age} years old',
+              style: const TextStyle(fontSize: 20),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
