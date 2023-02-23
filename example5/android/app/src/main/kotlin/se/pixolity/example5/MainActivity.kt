d611af43733bd45b2581ca7448919efbe6ea0461
Vandad Nahavandipoor <vandad.np@gmail.com>
2023-02-23 04:55:45 +0100

    example 5

---
+++ example5/android/app/src/main/kotlin/se/pixolity/example5/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.pixolity.example5
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example5/lib/main.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+const defaultWidth = 100.0;
+
+class _HomePageState extends State<HomePage> {
+  var _isZoomedIn = false;
+  var _buttonTitle = 'Zoom In';
+  var _width = defaultWidth;
+  var _curve = Curves.bounceOut;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Page'),
+      ),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          // Container
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              AnimatedContainer(
+                duration: const Duration(
+                  milliseconds: 370,
+                ),
+                width: _width,
+                curve: _curve,
+                child: Image.asset(
+                  'assets/images/wallpaper.jpg',
+                ),
+              ),
+            ],
+          ),
+          TextButton(
+            onPressed: () {
+              setState(() {
+                _isZoomedIn = !_isZoomedIn;
+                _buttonTitle = _isZoomedIn ? 'Zoom Out' : 'Zoom In';
+                _width = _isZoomedIn
+                    ? MediaQuery.of(context).size.width
+                    : defaultWidth;
+                _curve = _isZoomedIn ? Curves.bounceInOut : Curves.bounceOut;
+              });
+            },
+            child: Text(
+              _buttonTitle,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
