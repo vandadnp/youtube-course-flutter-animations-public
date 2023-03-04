7889729a4bc2bce004558a6f288a2a9b1991262d
Vandad Nahavandipoor <vandad.np@gmail.com>
2023-03-04 05:16:33 +0100

    example6

---
+++ example6/android/app/src/main/kotlin/se/pixolity/example6/MainActivity.kt
@@ -0,0 +1,6 @@
+package se.pixolity.example6
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ example6/lib/main.dart
@@ -0,0 +1,117 @@
+import 'package:flutter/material.dart';
+import 'dart:math' as math;
+
+import 'dart:developer' as devtools show log;
+
+extension Log on Object {
+  void log() => devtools.log(toString());
+}
+
+void main() {
+  runApp(
+    const App(),
+  );
+}
+
+class App extends StatelessWidget {
+  const App({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark),
+      darkTheme: ThemeData(brightness: Brightness.dark),
+      themeMode: ThemeMode.dark,
+      debugShowCheckedModeBanner: false,
+      debugShowMaterialGrid: false,
+      home: const HomePage(),
+    );
+  }
+}
+
+class CircleClipper extends CustomClipper<Path> {
+  const CircleClipper();
+
+  @override
+  Path getClip(Size size) {
+    var path = Path();
+
+    final rect = Rect.fromCircle(
+      center: Offset(size.width / 2, size.height / 2),
+      radius: size.width / 2,
+    );
+
+    path.addOval(rect);
+
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper<Path> oldClipper) => false;
+}
+
+/*
+0xFFFFFFFF
+A R G B = 32 bits
+  A = Alpha (0 - 255) - 8 bits
+  R = Red (0 - 255) - 8 bits
+  G = Green (0 - 255) - 8 bits
+  B = Blue (0 - 255) - 8 bits
+*/
+
+Color getRandomColor() => Color(
+      0xFF000000 +
+          math.Random().nextInt(
+            0x00FFFFFF,
+          ),
+    );
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var _color = getRandomColor();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ClipPath(
+          clipper: const CircleClipper(),
+          child: TweenAnimationBuilder(
+            tween: ColorTween(
+              begin: getRandomColor(),
+              end: _color,
+            ),
+            onEnd: () {
+              setState(() {
+                _color = getRandomColor();
+              });
+            },
+            duration: const Duration(seconds: 1),
+            child: Container(
+              width: MediaQuery.of(context).size.width,
+              height: MediaQuery.of(context).size.width,
+              color: Colors.red,
+            ),
+            builder: (context, Color? color, child) {
+              return ColorFiltered(
+                colorFilter: ColorFilter.mode(
+                  color!,
+                  BlendMode.srcATop,
+                ),
+                child: child,
+              );
+            },
+          ),
+        ),
+      ),
+    );
+  }
+}
